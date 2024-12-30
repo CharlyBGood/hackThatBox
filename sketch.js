4f820a56a3a46080cba50ed0dd49730d9d9e6888
charlybgood__x <bonavita.carlos@gmail.com>
2024-12-30 17:57:46 -0300

    start building audio context and functions to play sounds

--- sketch.js
+++ sketch.js
@@ -6,13 +6,57 @@ let boxHeight = 100;
 let createDesign = document.getElementById('create');
 let widthInput = document.getElementById('width');
 let heightInput = document.getElementById('height');
+// let audioCtx;
+// let osc;
+// let letsPlay = false;
+
+// function initAudioContext() {
+//   audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+// }
+
+// const NOTES = {
+//   "C-4": 261.626,
+//   "C-#": 277.1826,
+//   "D-4": 293.665,
+//   "D-#": 311.127,
+//   "E-4": 329.628,
+//   "F-4": 349.228,
+//   "F-#": 369.9944,
+//   "G-4": 391.995,
+//   "G-#": 415.3047,
+//   "A-4": 440.0,
+//   "A-#": 466.1638,
+//   "B-4": 493.883,
+//   "C-5": 523.251,
+// };
 
 
+// function playNote(note) {
+//   if (letsPlay) {
+//     initAudioContext();
+//     if (audioCtx.state === 'suspended') {
+//       audioCtx.resume();
+//     }
+//     osc = audioCtx.createOscillator();
+//     osc.type = "sine";
+//     osc.frequency.value = NOTES[note];
+//     osc.connect(audioCtx.destination);
+//     osc.start();
+//   }
+// }
+
+// function stopNote() {
+//   keyIsPressed = true;
+//   if (osc) {
+//     osc.stop();
+//     osc.disconnect();
+//   }
+// }
 
 createDesign.addEventListener('click', function () {
   fillColor = generateRandomColor();
   boxWidth = random(150, widthInput.value);
-  boxHeight = random(150, heightInput.value);  
+  boxHeight = random(150, heightInput.value);
 })
 
 function setup() {
@@ -22,10 +66,6 @@ function setup() {
 }
 
 function draw() {
-
-  // fill(0, 150, 220);
-  // circle(0, 0, circleSize);
-
   if (millis() - lastColorChange > 50) {
     fill(fillColor);
     lastColorChange = millis();
@@ -39,11 +79,13 @@ function draw() {
     fillColor = generateRandomColor();
     // noStroke();    
     fill(fillColor);
-    boxWidth = random(mouseY, 150);
-    boxHeight = random(mouseY, 75);
+    boxWidth = mouseX;
+    boxHeight = mouseY;
+    rotate(mouseX * 0.01);
+    // letsPlay = true;
+    // playNote('C-4');
     // cursor('none');    
-  }
-  // circle(mouseX - 200, mouseY - 200, 100);
+  }   
 }
 
 
